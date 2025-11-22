4dd20fb046ab5f99c5405c284a270a5dc6d76d57
Shibin Hussain MK <160138800+SHIBINSOJU@users.noreply.github.com>
2025-11-22 21:39:50 +0530

    Delete src directory

--- src/events/messageCreate.js
+++
@@ -1,9 +0,0 @@
-import { handleCountingMessage } from '../services/countingService.js';
-
-export default {
-  name: 'messageCreate',
-  async execute(message) {
-    if (message.author.bot) return;
-    await handleCountingMessage(message);
-  },
-};
